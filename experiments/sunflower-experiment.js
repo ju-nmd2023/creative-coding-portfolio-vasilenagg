0432399eb011b6bb87e22a66b4bc63a542355eca
vasilenagg <149382184+vasilenagg@users.noreply.github.com>
2025-09-03 16:10:45 +0200

    van gogh sunflower idea first try

--- experiments/sunflower-experiment.js
+++ experiments/sunflower-experiment.js
@@ -1,37 +1,36 @@
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  background(255, 255, 255);
-  colorMode(HSB);
+  background(217, 175, 50);
 }
 
-let flowerSize = 20;
-let amount = 4;
+let flowerSize = 50;
+let amount = 8;
 let gap = 90;
 
 function flower() {
   noStroke();
-  let petals = 5;
+  let petals = 19;
 
   for (let y = 0; y < petals; y++) {
     for (let x = 0; x < petals; x++) {
-      fill(136, 204, 4);
+      fill(123, 90, 22);
       rect(x, y, 40, 5);
 
-      fill(14, 24, 100);
+      fill(184, 148, 27);
       rect(x, y, 20, 15);
 
-      fill(255, 155, 130);
+      fill(168, 168, 57);
       ellipse(x, y, 3);
 
-      rotate(PI / 5);
+      rotate(PI / 7);
     }
   }
 }
 
 function draw() {
-  let y = (height - flowerSize * amount - gap * (amount - 1)) / 2;
+  let y = (height - flowerSize * amount - gap * (amount - 2)) / 2;
   for (i = 0; i < amount; i++) {
-    let x = (width - flowerSize * amount - gap * (amount - 1)) / 2;
+    let x = (width - flowerSize * amount - gap * (amount - 4)) / 2;
     for (let j = 0; j < amount; j++) {
       push();
       translate(x, y);
