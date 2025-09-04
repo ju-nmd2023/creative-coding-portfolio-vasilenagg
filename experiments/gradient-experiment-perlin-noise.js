d4309fe800fdda605675c09f2072a01acdcc1d3a
vasilenagg <149382184+vasilenagg@users.noreply.github.com>
2025-09-04 08:24:33 +0200

    original perlin noise

---
+++ experiments/gradient-experiment-perlin-noise.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+}
+
+const size = 10;
+const divider = 50;
+const numRows = 60;
+const numCols = 60;
+
+function draw() {
+  background(255, 255, 255);
+  noStroke();
+  fill(0, 0, 0);
+
+  //noiseSeed(0);
+  for (let y = 0; y < numRows; y++) {
+    for (let x = 0; x < numCols; x++) {
+      const value = noise(x / divider, y / divider) * size;
+      ellipse(size / 2 + x * size, size / 2 + y * size, value);
+    }
+  }
+  noLoop();
+}
