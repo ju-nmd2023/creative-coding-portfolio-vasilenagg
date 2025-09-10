2d1512d5a5698601974c991e29b076db4120be32
vasilenagg <149382184+vasilenagg@users.noreply.github.com>
2025-09-10 10:55:38 +0200

    live code lecture

---
+++ experiments/lecture-complexity-live-coding.js
@@ -0,0 +1,43 @@
+let position;
+let velocity;
+let acceleration;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  position = createVector(100, 100);
+  velocity = createVector(5, 8);
+  background(255);
+}
+
+function draw() {
+  noStroke();
+
+  push();
+  fill(15, random(255), 100);
+  ellipse(position.x, position.y, random(60));
+  ellipse(width - position.x, height - position.y, random(60));
+  pop();
+
+  push();
+  fill(80, random(255), 200);
+  ellipse(position.x, height - position.y, random(60));
+  ellipse(width - position.x, position.y, random(60));
+  pop();
+
+  if (position.x > width || position.x < 0) {
+    velocity.x *= -1;
+  }
+
+  if (position.y > height || position.y < 0) {
+    velocity.y *= -1;
+  }
+
+  const mouse = createVector(mouseX, mouseY);
+  acceleration = p5.Vector.sub(mouse, position);
+  acceleration.normalize();
+  acceleration.mult(0.5);
+
+  velocity.add(acceleration);
+  velocity.limit(10);
+  position.add(velocity);
+}
