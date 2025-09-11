f67c6c3facf6e559ab67f3818a1c52e72aa66393
vasilenagg <149382184+vasilenagg@users.noreply.github.com>
2025-09-11 17:32:45 +0200

    number matrix

---
+++ experiments/flow-field-matrix.js
@@ -0,0 +1,110 @@
+class Agent {
+  constructor(x, y, maxSpeed, maxForce) {
+    this.position = createVector(x, y);
+    this.lastPosition = createVector(x, y);
+    this.acceleration = createVector(0, 0);
+    this.velocity = createVector(0, 0);
+    this.maxSpeed = maxSpeed;
+    this.maxForce = maxForce;
+  }
+
+  follow(desiredDirection) {
+    desiredDirection = desiredDirection.copy();
+    desiredDirection.mult(this.maxSpeed);
+    let steer = p5.Vector.sub(desiredDirection, this.velocity);
+    steer.limit(this.maxForce);
+    this.applyForce(steer);
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  update() {
+    this.lastPosition = this.position.copy();
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0.2);
+  }
+
+  checkBorders() {
+    if (this.position.x < 0) {
+      this.position.x = innerWidth;
+      this.lastPosition.x = innerWidth;
+    } else if (this.position.x > innerWidth) {
+      this.position.x = 0;
+      this.lastPosition.x = 0;
+    }
+    if (this.position.y < 0) {
+      this.position.y = innerHeight;
+      this.lastPosition.y = innerHeight;
+    } else if (this.position.y > innerHeight) {
+      this.position.y = 0;
+      this.lastPosition.y = 0;
+    }
+  }
+
+  //code from https://p5js.org/reference/p5/random/
+  draw() {
+    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    let randomNumber = random(numbers);
+
+    fill(113, 219, 103);
+    text(randomNumber, this.lastPosition.x - 1, this.lastPosition.y);
+  }
+
+  // end of code from https://p5js.org/reference/p5/random/
+}
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(0);
+  field = generateField();
+  generateAgents();
+}
+
+function generateField() {
+  let field = [];
+  noiseSeed(Math.random() * 100);
+  for (let x = 0; x < maxCols; x++) {
+    field.push([]);
+    for (let y = 0; y < maxRows; y++) {
+      const value = noise(x / divider, y / divider) * Math.PI * 2;
+      field[x].push(p5.Vector.fromAngle(value));
+    }
+  }
+  return field;
+}
+
+function generateAgents() {
+  for (let i = 0; i < 130; i++) {
+    let agent = new Agent(
+      Math.random() * innerWidth,
+      Math.random() * innerHeight,
+      18,
+      5
+    );
+    agents.push(agent);
+  }
+}
+
+const fieldSize = 5000;
+const maxCols = Math.ceil(innerWidth / fieldSize);
+const maxRows = Math.ceil(innerHeight / fieldSize);
+const divider = 20;
+let field;
+let agents = [];
+
+function draw() {
+  for (let agent of agents) {
+    const x = Math.floor(agent.position.x / fieldSize);
+    const y = Math.floor(agent.position.y / fieldSize);
+    const desiredDirection = field[x][y];
+    agent.follow(desiredDirection);
+    agent.update();
+    agent.checkBorders();
+    agent.draw();
+  }
+}
